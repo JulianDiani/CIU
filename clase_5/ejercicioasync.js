669429608b74460de32f7b11da0189598b185be1
JulianDiani <juliandiani95@gmail.com>
2021-10-19 18:11:15 -0300

    agregado ejercicio async

---
+++ clase_5/ejercicioasync.js
@@ -0,0 +1,19 @@
+function getProcessedData(url) {
+  return downloadData(url) // returns a promise
+    .catch(e => {
+      return downloadFallbackData(url)  // returns a promise
+    })
+    .then(v => {
+      return processDataInWorker(v); // returns a promise
+    });
+}
+
+async function getProcessedData2(url){
+    try{
+        const downloadedData = await downloadData(url);
+        const processDataInWorker= await proccesDataInWorker(v)
+    }
+    catch(e){
+        const downloadFallbackData= await downloadFallbackData(url)
+    }
+}
